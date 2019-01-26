c907c48ccc5d97990909a09cd0ffe1f38b944aad
john reilly <johnreillymct@outlook.com>
2019-01-26 14:32:52 +0000

    debug in main.cpp

--- src/MPC.h
+++ src/MPC.h
@@ -12,8 +12,9 @@ class MPC {
 
   // Solve the model given an initial state and polynomial coefficients.
   // Return the first actuations.
-  std::vector<double> Solve(const Eigen::VectorXd &state, 
-                            const Eigen::VectorXd &coeffs);
+ //no & or const in video and maybe causing index error
+  //std::vector<double> Solve(const Eigen::VectorXd &state,  const Eigen::VectorXd &coeffs);
+  std::vector<double> Solve(Eigen::VectorXd state,  Eigen::VectorXd coeffs);
 };
 
 #endif  // MPC_H
